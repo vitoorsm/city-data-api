4a81385cbe08276d5c045034ae25d55a9fe6fd06
vitoorsm <vitoorms713@gmail.com>
2023-03-21 10:06:27 -0300

    Add formatResponseToCurrentWeatherClass method

--- src/main/java/com/citydata/services/CityDataService.java
+++ src/main/java/com/citydata/services/CityDataService.java
@@ -1,10 +1,25 @@
 package com.citydata.services;
 
+import com.citydata.models.CurrentWeather;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Service;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+@Service
+@AllArgsConstructor
 public class CityDataService {
     private String getCurrentWeatherDataResponse(String city, String countryCode, String API_KEY) {
         try {
@@ -23,6 +38,36 @@ public class CityDataService {
             scanner.close();
             return response.toString();
         }catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private CurrentWeather formatResponseToCurrentWeatherClass(String response) {
+        try {
+            JSONParser parse = new JSONParser();
+            JSONObject dataObj = (JSONObject) parse.parse(response);
+            JSONObject mainData = (JSONObject) dataObj.get("main");
+            JSONObject sysData = (JSONObject) dataObj.get("sys");
+            JSONArray weatherData = (JSONArray) dataObj.get("weather");
+
+            Pattern pattern = Pattern.compile("description");
+            Matcher matcher = pattern.matcher(weatherData.toString());
+            String weatherDescription = "";
+
+            while (matcher.find()){
+                String cutString = weatherData.toString().substring(matcher.end()+3, 60);
+                weatherDescription = cutString.substring(0, cutString.indexOf(",")-1);
+            }
+
+            Gson gson = new GsonBuilder().create();
+            CurrentWeather currentWeather = gson.fromJson(String.valueOf(mainData), CurrentWeather.class);
+            currentWeather.setCity((String) dataObj.get("name"));
+            currentWeather.setCountry(gson.fromJson(String.valueOf(sysData.get("country")), String.class));
+            currentWeather.setDescription(StringUtils.capitalize(weatherDescription));
+            return currentWeather;
+
+        }
+        catch (ParseException e){
             e.printStackTrace();
         }
         return null;
