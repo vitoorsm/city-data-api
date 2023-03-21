bb05135ef333a625c7294bafaa5e422f3731ab88
vitoorsm <vitoorms713@gmail.com>
2023-03-21 07:11:25 -0300

    first commit

---
+++ src/test/java/com/citydata/CityDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
