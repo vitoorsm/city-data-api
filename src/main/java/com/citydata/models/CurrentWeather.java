cae0fc536369ba28fc4d105577a07932eb952ec1
vitoorsm <vitoorms713@gmail.com>
2023-03-21 09:36:28 -0300

    Add models package and CurrentWeather class

---
+++ src/main/java/com/citydata/models/CurrentWeather.java
@@ -0,0 +1,17 @@
+package com.citydata.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CurrentWeather {
+    private String city;
+    private String country;
+    private String temp;
+    private String feels_like;
+    private String temp_min;
+    private String temp_max;
+    private String humidity;
+    private String description;
+}
