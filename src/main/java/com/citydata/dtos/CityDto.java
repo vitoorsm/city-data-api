581d59adfed8acd9cf6bacc77ed381a6ad5ec2a1
vitoorsm <vitoorms713@gmail.com>
2023-03-23 04:16:48 -0300

    Implement validation annotations

--- src/main/java/com/citydata/dtos/CityDto.java
+++ src/main/java/com/citydata/dtos/CityDto.java
@@ -1,9 +1,14 @@
 package com.citydata.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CityDto {
+    @NotBlank(message = "city must not be blank")
     private String city;
+    @NotBlank(message = "country must not be blank")
+    @Size(min = 2, max = 2)
     private String country;
 }
